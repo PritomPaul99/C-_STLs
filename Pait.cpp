d8c90d7f6a2d9de6826a605935014bbd84062859
Pritom Paul <pritompaul1920@gmail.com>
2022-05-19 00:29:35 +0600

    Added map and pair, modifisd List

---
+++ Pait.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+
+
+int main()
+{
+    pair<int, string> p1;
+
+    p1.first = 1;
+    p1.second = "Pritom";
+    
+    return 0;
+}
+
+
+
